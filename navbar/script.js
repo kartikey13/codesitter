5c622f8462b912a3c0f1cdcb0871a08d3cd21bf2
Kartikey Singh Rawat <37929281+kartikey13@users.noreply.github.com>
2021-06-03 02:31:57 +0530

    Update script.js

--- navbar/script.js
+++ navbar/script.js
@@ -1,8 +1,26 @@
-var parent = document.querySelectorAll('.navbar-item');
-var drop = document.querySelector('.dd-menu');
+window.onload = () => {
+var navbaritem = document.querySelectorAll('.navbar-item');
+var dropdown = document.querySelectorAll('.dd-start');
 
-parent.forEach(function(parent_item) {
-    parent_item.onclick = function(){
-        parent_item.classList.add('active');
-    }
-});
+//the main dropdown, if uh click any items on the navbar then the first dropdown will open
+navbaritem.forEach(function (navbaritem_item){
+    navbaritem_item.addEventListener("click", () => {
+        navbaritem.forEach(function (navbaritem_item){
+            navbaritem_item.classList.remove('active');
+        })        
+        navbaritem_item.classList.add('active');    
+
+    })
+})
+//for the dropdown menu to appear, we add active class to every dd-start which is the first dropdown.
+dropdown.forEach(function (dropdown_item){
+    console.log(dropdown_item);
+    dropdown_item.addEventListener("click", () => {
+        dropdown.forEach(function (dropdown_item){
+            dropdown_item.classList.remove('active');
+        })        
+        dropdown_item.classList.add('active');    
+    })
+})
+
+}
